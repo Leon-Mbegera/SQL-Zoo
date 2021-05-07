7a6741d88bdb0b9a1185c6c0a9ef73b0fa1b74a1
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-05-07 13:13:53 +0300

    Select quiz

--- file.sql
+++ file.sql
@@ -8,4 +8,31 @@ WHERE name IN ('Sweden', 'Norway', 'Denmark');
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
 
+SELECT name, population
+  FROM world
+ WHERE population BETWEEN 1000000 AND 1250000
+
+Table-E
+Albania	3200000
+Algeria	32900000
+
+SELECT name FROM world
+ WHERE name LIKE '%a' OR name LIKE '%l'
+
+name	length(name)
+Italy	5
+Malta	5
+Spain	5
+
+Andorra	936
+
+SELECT name, area, population
+  FROM world
+ WHERE area > 50000 AND population < 10000000
+
+
+SELECT name, population/area
+  FROM world
+ WHERE name IN ('China', 'Nigeria', 'France', 'Australia')
+
   
