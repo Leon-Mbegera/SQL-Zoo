19ca801ce4df1a593874964ccc907dc4c81aef24
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-05-10 16:50:40 +0300

    Nobel Quiz

--- file.sql
+++ file.sql
@@ -179,4 +179,35 @@ CASE WHEN subject IN ('Physics', 'Chemistry') THEN 1 ELSE 0 END,
  subject, winner
 
 
-  
+SELECT winner FROM nobel
+ WHERE winner LIKE 'C%' AND winner LIKE '%n'
+
+SELECT COUNT(subject) FROM nobel
+ WHERE subject = 'Chemistry'
+   AND yr BETWEEN 1950 and 1960
+
+   SELECT COUNT(DISTINCT yr) FROM nobel
+ WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine')
+
+ Medicine	Sir John Eccles
+Medicine	Sir Frank Macfarlane Burnet
+
+SELECT yr FROM nobel
+ WHERE yr NOT IN(SELECT yr 
+                   FROM nobel
+                 WHERE subject IN ('Chemistry','Physics'))
+
+SELECT DISTINCT yr
+  FROM nobel
+ WHERE subject='Medicine' 
+   AND yr NOT IN(SELECT yr FROM nobel 
+                  WHERE subject='Literature')
+   AND yr NOT IN (SELECT yr FROM nobel
+                   WHERE subject='Peace')
+
+
+Chemistry	1
+Literature	1
+Medicine	2
+Peace	1
+Physics	1                                    
