29c6482b461b274eb351dfadac295d6d495de24b
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-05-11 18:07:50 +0300

    Join Quiz

--- file.sql
+++ file.sql
@@ -408,3 +408,32 @@ team2,
   FROM game JOIN goal ON matchid = id GROUP BY mdate,matchid,team1,team2
 ORDER BY mdate,matchid,team1,team2
 
+game  JOIN goal ON (id=matchid)
+
+matchid, teamid, player, gtime, id, teamname, coach
+
+SELECT player, teamid, COUNT(*)
+  FROM game JOIN goal ON matchid = id
+ WHERE (team1 = "GRE" OR team2 = "GRE")
+   AND teamid != 'GRE'
+ GROUP BY player, teamid
+
+DEN	9 June 2012
+GER	9 June 2012
+
+SELECT DISTINCT player, teamid 
+   FROM game JOIN goal ON matchid = id 
+  WHERE stadium = 'National Stadium, Warsaw' 
+ AND (team1 = 'POL' OR team2 = 'POL')
+   AND teamid != 'POL'
+
+SELECT DISTINCT player, teamid, gtime
+  FROM game JOIN goal ON matchid = id
+ WHERE stadium = 'Stadion Miejski (Wroclaw)'
+   AND (( teamid = team2 AND team1 != 'ITA') OR ( teamid = team1 AND team2 != 'ITA'))
+
+Netherlands	2
+Poland	2
+Republic of Ireland	1
+Ukraine	2
+   
