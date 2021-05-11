7c6c684d530f9030a6e3002552cdd9b32e366769
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-05-11 17:47:24 +0300

    The JOIN Operation

--- file.sql
+++ file.sql
@@ -349,3 +349,62 @@ Middle East	13403102
 South America	17740392
 South Asia	9437710
 
+SELECT matchid, player 
+FROM goal 
+  WHERE teamid = 'GER'
+
+SELECT id,stadium,team1,team2
+  FROM game
+   WHERE id = '1012'
+
+SELECT player,teamid,stadium,mdate
+  FROM game JOIN goal ON (id=matchid)
+WHERE teamid ='GER'
+
+SELECT team1, team2, player
+FROM game JOIN goal on (id=matchid)
+WHERE player LIKE 'Mario%'
+
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON (teamid=id) 
+ WHERE gtime<=10
+
+
+SELECT mdate, teamname
+FROM game JOIN eteam ON (team1=eteam.id)
+WHERE coach = 'Fernando Santos'
+
+SELECT player
+FROM game JOIN goal ON (game.id = goal.matchid)
+WHERE stadium = 'National Stadium, Warsaw'
+
+SELECT DISTINCT player
+FROM game JOIN goal ON matchid = id 
+WHERE (team1='GER' OR team2='GER')
+AND teamid <> 'GER'
+
+SELECT teamname, COUNT(player)
+  FROM eteam JOIN goal ON id=teamid
+   GROUP BY teamname
+
+SELECT stadium, COUNT(player)
+FROM game JOIN goal ON (game.id=goal.matchid)
+GROUP BY stadium
+
+SELECT matchid,mdate, COUNT(mdate)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL') AND id = matchid GROUP BY mdate,matchid
+
+SELECT matchid, mdate, COUNT(player)
+FROM game JOIN goal ON (matchid=id)
+WHERE teamid = 'GER'
+GROUP BY matchid, mdate
+
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) AS score1,
+team2,
+  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
+  FROM game JOIN goal ON matchid = id GROUP BY mdate,matchid,team1,team2
+ORDER BY mdate,matchid,team1,team2
+
