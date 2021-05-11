9e0cecb96aa6a4c67bf681ccc64e7036012fae66
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-05-11 13:26:56 +0300

    Nested Select Quiz

--- file.sql
+++ file.sql
@@ -269,3 +269,31 @@ FROM world y
 WHERE x.continent = y.continent
 AND x.name <> y.name  
 )
+
+SELECT region, name, population FROM bbc x WHERE population <= ALL (SELECT population FROM bbc y WHERE y.region=x.region AND population>0)
+
+SELECT name,region,population FROM bbc x WHERE 50000 < ALL (SELECT population FROM bbc y WHERE x.region=y.region AND y.population>0)
+
+SELECT name, region FROM bbc x
+ WHERE population < ALL (SELECT population/3 FROM bbc y WHERE y.region = x.region AND y.name != x.name)
+
+Table-D
+France
+Germany
+Russia
+Turkey
+
+SELECT name FROM bbc
+ WHERE gdp > (SELECT MAX(gdp) FROM bbc WHERE region = 'Africa')
+
+
+SELECT name FROM bbc
+ WHERE population < (SELECT population FROM bbc WHERE name='Russia')
+   AND population > (SELECT population FROM bbc WHERE name='Denmark')
+
+
+Table-B
+Bangladesh
+India
+Pakistan
+
