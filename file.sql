d4fe0fc1523116688158d351a2b4d819528bc53f
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-05-11 12:23:31 +0300

    Select in select

--- file.sql
+++ file.sql
@@ -210,4 +210,62 @@ Chemistry	1
 Literature	1
 Medicine	2
 Peace	1
-Physics	1                                    
+Physics	1
+
+SELECT name FROM world
+WHERE population > (SELECT population FROM world WHERE name='Russia')
+
+SELECT name
+FROM world
+WHERE continent = 'Europe' AND 
+GDP/population > (SELECT GDP/population FROM world WHERE name = 'United Kingdom')
+
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent FROM world WHERE name IN('Argentina', 'Australia'))
+ORDER BY name
+
+
+SELECT name, population
+FROM world
+WHERE population > (SELECT population FROM world WHERE name = 'Canada')
+AND population < (SELECT population FROM world WHERE name = 'Poland')
+
+
+SELECT name, CONCAT(
+               ROUND(
+                 100*population/
+(SELECT population FROM world WHERE name = 'Germany'), 0),'%')
+FROM world
+WHERE continent = 'Europe'
+
+
+SELECT name FROM world
+WHERE gdp >
+(SELECT MAX(gdp) FROM world WHERE continent = 'Europe')
+
+
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
+
+
+SELECT continent, name FROM world x
+WHERE name <= ALL (SELECT name FROM world y 
+WHERE y.continent=x.continent AND area > 0)
+
+SELECT name, continent, population FROM world x 
+WHERE 25000000 >= ALL (SELECT population FROM world y
+                        WHERE x.population=y.population
+                         AND area>0)
+
+
+SELECT name,continent FROM world x
+WHERE population >= ALL(
+SELECT population*3
+FROM world y
+WHERE x.continent = y.continent
+AND x.name <> y.name  
+)
