96ae2f5909a5ffc6fd198c078cb50c65a56e9675
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-05-11 15:39:37 +0300

    Sum and Count Quiz

--- file.sql
+++ file.sql
@@ -328,3 +328,24 @@ SELECT continent
 FROM world
 GROUP BY continent
 HAVING SUM(population) >= 100000000
+
+SELECT SUM(population) FROM bbc WHERE region = 'Europe'
+
+SELECT COUNT(name) FROM bbc WHERE population < 150000
+
+AVG(), COUNT(), MAX(), MIN(), SUM()
+
+No result due to invalid use of the WHERE function
+
+SELECT AVG(population) FROM bbc WHERE name IN ('Poland', 'Germany', 'Denmark')
+
+SELECT region, SUM(population)/SUM(area) AS density FROM bbc GROUP BY region
+
+SELECT name, population/area AS density FROM bbc WHERE population = (SELECT MAX(population) FROM bbc)
+
+Table-D
+Americas	732240
+Middle East	13403102
+South America	17740392
+South Asia	9437710
+
